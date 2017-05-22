ef45e3b2a597667dc910dbcd65ded53c46eea7d8
student11510 <study11510@gmail.com>
2017-05-22 15:41:17 +0000

    CRUD task

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,69 @@
+class TasksController < ApplicationController
+  def index
+    @taskall = Task.all
+  end
+
+  def show
+    @taskfind_id = Task.find(params[:id])
+  end
+  
+  def new
+    @tasknew = Task.new
+  end
+
+  def create
+    @task_create = Task.new(task_params)
+    
+    if @task_create.save
+      flash[:success] = 'タスク が正常に投稿されました'
+      redirect_to @task_create
+    else
+      flash.now[:danger] = 'タスク が投稿されませんでした'
+      render :new
+    end
+  end 
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = 'タスクが正常に投稿されました。'
+      redirect_to task_url(@task)
+    else
+      flash.now[:danger] = 'タスクが投稿されませんでした。'
+      render :edit
+    end
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = 'タスクは正常に削除されました。'
+    redirect_to root_path
+  end
+  
+  
+    def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+
+    flash[:success] = 'Message は正常に削除されました'
+    redirect_to tasks_url
+    
+    end
+  
+  
+  
+  private
+
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content)
+  end
+
+end
