8c0f129890d0c936250423de2e73bd3bc7f328c5
student11510 <study11510@gmail.com>
2017-05-25 06:44:26 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,22 +1,25 @@
 class TasksController < ApplicationController
+  
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
     @taskall = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+
   end
   
   def new
-    @tasknew = Task.new
+    @task = Task.new
   end
 
   def create
-    @task_create = Task.new(task_params)
+    @task = Task.new(task_params)
     
-    if @task_create.save
+    if @task.save
       flash[:success] = 'タスク が正常に投稿されました'
-      redirect_to @task_create
+      redirect_to @task
     else
       flash.now[:danger] = 'タスク が投稿されませんでした'
       render :new
@@ -24,12 +27,11 @@ class TasksController < ApplicationController
   end 
 
   def edit
-    @task = Task.find(params[:id])
+
   end
 
   def update
-    @task = Task.find(params[:id])
-    
+
     if @task.update(task_params)
       flash[:success] = 'タスクが正常に投稿されました。'
       redirect_to task_url(@task)
@@ -40,8 +42,8 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @taskd = Task.find(params[:id])
-    @taskd.destroy
+
+    @task.destroy
     
     flash[:success] = 'タスクは正常に削除されました。'
     redirect_to root_path
@@ -49,10 +51,15 @@ class TasksController < ApplicationController
   
   
   private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
+    
 
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 
 end
